e402b59e1fa421b266945b840c9ba642c47b36fd
Ye Htet Maung Maung <yehtetmgmgse@gmail.com>
2023-01-11 09:26:14 +0630

    map comprehension

--- live/prod/services/webserver-cluster/outputs.tf
+++ live/prod/services/webserver-cluster/outputs.tf
@@ -10,3 +10,7 @@ output "upper_names" {
 output "bios" {
   value = [for name, role in var.hero_thousand_faces : "${name} is the ${role}"]
 }
+
+output "upper_roles" {
+  value = {for name, role in var.hero_thousand_faces : upper(name) => upper(role)}
+}
