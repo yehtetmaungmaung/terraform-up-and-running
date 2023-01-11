8d5cce3bc607765ca69f99032c64c566b5672f53
Ye Htet Maung Maung <yehtetmgmgse@gmail.com>
2023-01-11 08:50:29 +0630

    custom tags

--- live/prod/services/webserver-cluster/main.tf
+++ live/prod/services/webserver-cluster/main.tf
@@ -1,38 +1,43 @@
 provider "aws" {
-    region = "us-east-2"
+  region = "us-east-2"
 }
 
 terraform {
   backend "s3" {
     bucket = "terraform-up-and-running-state-by-yehtetmaungmaung"
-    key = "prod/services/webserver-cluster/terraform.tfstate"
+    key    = "prod/services/webserver-cluster/terraform.tfstate"
     region = "us-east-2"
 
     dynamodb_table = "terraform-up-and-running-locks"
-    encrypt = true
+    encrypt        = true
   }
 }
 
 module "webserver_cluster" {
-    source = "../../../../modules/services/webserver-cluster"
+  source = "../../../../modules/services/webserver-cluster"
 
-    cluster_name = "webservers-prod"
-    db_remote_state_bucket = "terraform-up-and-running-state-by-yehtetmaungmaung"
-    db_remote_state_key = "prod/data-stores/mysql/terraform.tfstate"
+  cluster_name           = "webservers-prod"
+  db_remote_state_bucket = "terraform-up-and-running-state-by-yehtetmaungmaung"
+  db_remote_state_key    = "prod/data-stores/mysql/terraform.tfstate"
 
-    instance_type = "t2.micro"
-    min_size      = 2
-    max_size      = 3
-    server_port = 8080
+  instance_type = "t2.micro"
+  min_size      = 2
+  max_size      = 3
+  server_port   = 8080
+
+  custom_tags = {
+    Owner     = "team-foo"
+    ManagedBy = "terraform"
+  }
 
 }
 
 resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
-  scheduled_action_name = "scale-out-during-business-hours"
-  min_size              = 2
-  max_size              = 10
-  desired_capacity      = 10
-  recurrence            = "0 9 * * *"
+  scheduled_action_name  = "scale-out-during-business-hours"
+  min_size               = 2
+  max_size               = 10
+  desired_capacity       = 10
+  recurrence             = "0 9 * * *"
   autoscaling_group_name = module.webserver_cluster.asg_name
 }
 
--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -3,18 +3,18 @@ data "terraform_remote_state" "db" {
 
   config = {
     bucket = var.db_remote_state_bucket
-    key = var.db_remote_state_key
+    key    = var.db_remote_state_key
     region = "us-east-2"
-  }  
+  }
 }
 
 data "aws_vpc" "default" {
   default = true
-}   
+}
 
 data "aws_subnets" "default" {
   filter {
-    name = "vpc-id"
+    name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
 }
@@ -39,15 +39,15 @@ data "aws_subnets" "default" {
 #}
 
 resource "aws_launch_configuration" "example" {
-  image_id = "ami-0fb653ca2d3203ac1"
-  instance_type = var.instance_type
+  image_id        = "ami-0fb653ca2d3203ac1"
+  instance_type   = var.instance_type
   security_groups = [aws_security_group.instance.id]
 
   # Render the User Data script as a template
   user_data = templatefile("${path.module}/user-data.sh", {
     server_port = var.server_port
-    db_address = data.terraform_remote_state.db.outputs.address
-    db_port = data.terraform_remote_state.db.outputs.port
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
   })
 
   # Required when using a launch configuration with an auto scaling group.
@@ -59,7 +59,7 @@ resource "aws_launch_configuration" "example" {
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier = data.aws_subnets.default.ids
+  vpc_zone_identifier  = data.aws_subnets.default.ids
 
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
@@ -69,23 +69,33 @@ resource "aws_autoscaling_group" "example" {
   max_size = var.max_size
 
   tag {
-    key = "Name"
-    value = var.cluster_name
+    key                 = "Name"
+    value               = var.cluster_name
     propagate_at_launch = true
+  }
+
+  dynamic "tag" {
+    for_each = var.custom_tags
+
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
   }
 }
 
 resource "aws_lb" "example" {
-  name = "${var.cluster_name}-alb"
+  name               = "${var.cluster_name}-alb"
   load_balancer_type = "application"
-  subnets = data.aws_subnets.default.ids
-  security_groups = [aws_security_group.alb.id]
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
 }
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
-  port = local.http_port
-  protocol = "HTTP"
+  port              = local.http_port
+  protocol          = "HTTP"
 
   default_action {
     type = "fixed-response"
@@ -93,7 +103,7 @@ resource "aws_lb_listener" "http" {
     fixed_response {
       content_type = "text/plain"
       message_body = "404: Page Not Found"
-      status_code = 404
+      status_code  = 404
     }
   }
 }
@@ -136,9 +146,9 @@ resource "aws_security_group" "instance" {
   name = "${var.cluster_name}-instance"
 
   ingress {
-    from_port = var.server_port
-    to_port = var.server_port
-    protocol = "tcp"
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -148,7 +158,7 @@ resource "aws_security_group" "alb" {
 }
 
 resource "aws_security_group_rule" "allow_http_inbound" {
-  type = "ingress"
+  type              = "ingress"
   security_group_id = aws_security_group.alb.id
 
   from_port   = local.http_port
@@ -158,13 +168,13 @@ resource "aws_security_group_rule" "allow_http_inbound" {
 }
 
 resource "aws_security_group_rule" "allow_all_outbound" {
-    type = "egress"
-    security_group_id = aws_security_group.alb.id
+  type              = "egress"
+  security_group_id = aws_security_group.alb.id
 
-    from_port   = local.any_port
-    to_port     = local.any_port
-    protocol    = local.any_protocol
-    cidr_blocks = local.all_ips
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ips
 
 }
 
